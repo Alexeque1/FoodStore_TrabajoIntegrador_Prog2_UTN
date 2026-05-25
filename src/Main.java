d16a372bb232acba0488f60fa88d3ba3508ade0d
Alejandro Ruiz Diaz <alee_ruizdiaz@hotmail.com>
2026-05-25 17:17:43 -0300

    test verificacion flujo rama dev-Alejandro

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import ui.MenuPrincipal;
 public class Main {
-
+    
     public static void main(String[] args) {
+        
+        // Desarrollado por: Alexander Sequera y Alejandro Ruiz Diaz
+        
         MenuPrincipal menu = new MenuPrincipal();
         menu.iniciar();
     }
